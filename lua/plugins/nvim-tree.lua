ad4819150663357ddce11314d1fed5bdee81401d
bhagwan <bhagwan@disroot.org>
2021-09-29 12:01:43 -0700

    nvim-tree breaking changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -26,21 +26,6 @@ vim.g.nvim_tree_show_icons = {
   lsp = 1,
 }
 
-vim.g.nvim_tree_add_trailing = 0
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_follow = 0
-vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_group_empty = 0
-vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_root_folder_modifier = ":~"
-vim.g.nvim_tree_side = "left"
-vim.g.nvim_tree_tab_open = 0
-vim.g.nvim_tree_width = 30
-
 -- Mostly default mappings
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
@@ -115,4 +100,62 @@ vim.g.nvim_tree_icons = {
     warning = "",
     error   = "",
   },
+}
+
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = false,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = true,
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  lsp_diagnostics     = false,
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  view = {
+    width = 30,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+        { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
+        { key = "<C-v>",                        cb = tree_cb("vsplit") },
+        { key = "<C-s>",                        cb = tree_cb("split") },
+        { key = "<C-t>",                        cb = tree_cb("tabnew") },
+        { key = "<",                            cb = tree_cb("prev_sibling") },
+        { key = ">",                            cb = tree_cb("next_sibling") },
+        { key = "P",                            cb = tree_cb("parent_node") },
+        { key = "<BS>",                         cb = tree_cb("close_node") },
+        { key = "<S-CR>",                       cb = tree_cb("close_node") },
+        { key = "<Tab>",                        cb = tree_cb("preview") },
+        { key = "K",                            cb = tree_cb("first_sibling") },
+        { key = "J",                            cb = tree_cb("last_sibling") },
+        { key = "I",                            cb = tree_cb("toggle_ignored") },
+        { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+        { key = "R",                            cb = tree_cb("refresh") },
+        { key = "a",                            cb = tree_cb("create") },
+        { key = "d",                            cb = tree_cb("remove") },
+        { key = "r",                            cb = tree_cb("rename") },
+        { key = "<C-r>",                        cb = tree_cb("full_rename") },
+        { key = "x",                            cb = tree_cb("cut") },
+        { key = "c",                            cb = tree_cb("copy") },
+        { key = "p",                            cb = tree_cb("paste") },
+        { key = "y",                            cb = tree_cb("copy_name") },
+        { key = "Y",                            cb = tree_cb("copy_path") },
+        { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+        { key = "[c",                           cb = tree_cb("prev_git_item") },
+        { key = "]c",                           cb = tree_cb("next_git_item") },
+        { key = "-",                            cb = tree_cb("dir_up") },
+        { key = "s",                            cb = tree_cb("system_open") },
+        { key = "q",                            cb = tree_cb("close") },
+      }
+    }
+  }
 }
