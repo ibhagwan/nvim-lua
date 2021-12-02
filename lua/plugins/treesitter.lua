3d9c9fca2612f50bf20e9d96ef2d63e0bdb98f36
bhagwan <bhagwan@disroot.org>
2021-12-02 10:12:54 -0800

    treesitter init_selection bind changed to <CR> (conflicted with <C-i>)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -30,7 +30,7 @@ require'nvim-treesitter.configs'.setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<Tab>',
+      init_selection = '<CR>',
       scope_incremental = '<CR>',
       node_incremental = '<Tab>',
       node_decremental = '<S-Tab>',
