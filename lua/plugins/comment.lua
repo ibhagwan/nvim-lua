93fa363e27a9e7baefe503b903ceb24f021b4dba
bhagwan <bhagwan@disroot.org>
2022-01-03 21:33:11 -0800

    comment.nvim config update

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,11 +5,30 @@ end
 
 comment.setup({
 
+    padding = true,       -- Add a space b/w comment and the line
+    sticky = true,        -- Whether the cursor should stay at its position
+
+    -- NORMAL mode line-wise mappings
+    toggler = {
+        line = 'gcc',
+        block = 'gbc',
+    },
+
+    -- NORMAL+VISUAL mode operator mappings
     opleader = {
         line = 'gl',
         block = 'gc',
     },
 
-    pre_hook = nil,
-    post_hook = nil,
+    extra = {
+        above = 'gcO',    -- Add comment on the line above
+        below = 'gco',    -- Add comment on the line below
+        eol = 'gcA',      -- Add comment at the end of line
+    },
+
+    mappings = {
+        basic = true,     -- Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
+        extra = true,     -- Includes `gco`, `gcO`, `gcA`
+        extended = false, -- Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+    },
 })
