0658711a7ea271456760328fecddee60a9a9f566
bhagwan <bhagwan@disroot.org>
2022-01-15 07:58:15 -0800

    added missing workdirs.lua

---
+++ lua/workdirs.lua
@@ -0,0 +1,15 @@
+return {
+  vim.loop.cwd(),
+  '~/dots',
+  '~/.config/nvim',
+  '~/.config/zsh',
+  '~/.config/awesome',
+  '~/Sources/nvim/fzf-lua',
+  '~/Sources/nvim/fzf-lua.wiki',
+  '~/Sources/nvim/nvim-lua',
+  '~/Sources/nvim/nvim-fzf',
+  '~/Sources/nvim/plenary.nvim',
+  '~/Sources/nvim/telescope.nvim',
+  '~/Sources/nvim/babelfish.nvim',
+  vim.fn.stdpath("data") .. "/site/pack/packer",
+}
