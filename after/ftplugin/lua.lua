d5f375b19c63727754e046a7e157b91ce3f50274
bhagwan <bhagwan@disroot.org>
2022-02-11 09:00:37 -0800

    set nospell for all ftplugin filetypes

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,3 +1,4 @@
+vim.wo.spell       = false
 vim.bo.shiftwidth  = 2
 vim.bo.tabstop     = 2
 vim.bo.softtabstop = 2
--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,3 +1,4 @@
+vim.wo.spell       = false
 vim.bo.shiftwidth  = 4
 vim.bo.tabstop     = 4
 vim.bo.softtabstop = 4
