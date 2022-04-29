4d9e5c8dabf4439e36e5f45660b79c5b706edec1
bhagwan <bhagwan@disroot.org>
2022-04-28 23:04:32 -0700

    lspconfig: nvim-lsp-installer 'on_server_ready' deprecation update

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,53 +1,83 @@
-if not pcall(require, "lspconfig") or not pcall(require, "nvim-lsp-installer") then
-  return
-end
+-- setup nvim-lsp-installer before interacting with lspconfig
+-- https://github.com/williamboman/nvim-lsp-installer/discussions/636
+pcall(function()
+  require("nvim-lsp-installer").setup({
+    ensure_installed = { "sumneko_lua" },
+    ui = {
+      icons = {
+        server_installed = "✓",
+        server_pending = "➜",
+        server_uninstalled = "✗"
+      }
+    }
+  })
+end)
+
+local ok, lspconfig = pcall(require, "lspconfig")
+if not ok then return end
 
 -- Setup icons & handler helper functions
 require('lsp.diag')
 require('lsp.icons')
 require('lsp.handlers')
 
--- default 'on_attach' function
-local on_attach = require('lsp.on_attach').on_attach
-
 -- Enable borders for hover/signature help
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+vim.lsp.handlers['textDocument/hover'] =
+  vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+vim.lsp.handlers['textDocument/signatureHelp'] =
+  vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+
+local __settings = {}
 
 -- Lua settings
-local lua_settings = {
-  Lua = {
-    telemetry = { enable = false },
-    runtime = {
-      -- LuaJIT in the case of Neovim
-      version = 'LuaJIT',
-      path = vim.split(package.path, ';'),
-    },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = {
-        'vim',
-        'root',         -- awesomeWM
-        'awesome',      -- awesomeWM
-        'screen',       -- awesomeWM
-        'client',       -- awesomeWM
-        'clientkeys',   -- awesomeWM
-        'clientbuttons',-- awesomeWM
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+__settings['sumneko_lua'] = {
+  -- enables sumneko_lua formatting, see:
+  -- https://github.com/sumneko/lua-language-server/issues/960
+  -- https://github.com/sumneko/lua-language-server/wiki/Code-Formatter
+  cmd = { 'lua-language-server', '--preview' },
+  -- no need to override 'root_dir' since we added '.config/nvim/.luacheckrc'
+  -- root_dir = require'lspconfig.util'.root_pattern(
+  --   ".luarc.json", ".luacheckrc", ".stylua.toml", "selene.toml", ".git", ".sumneko_lua"),
+  settings = {
+    Lua = {
+      telemetry = { enable = false },
+      runtime = {
+        -- LuaJIT in the case of Neovim
+        version = 'LuaJIT',
+        path = runtime_path,
       },
-      -- enables formatter warnings
-      -- neededFileStatus = { ['codestyle-check'] = "any" }
-    },
-    workspace = {
-      -- Make the server aware of Neovim runtime files
-      library = {
-        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'root',         -- awesomeWM
+          'awesome',      -- awesomeWM
+          'screen',       -- awesomeWM
+          'client',       -- awesomeWM
+          'clientkeys',   -- awesomeWM
+          'clientbuttons',-- awesomeWM
+        },
+        -- enables formatter warnings
+        -- neededFileStatus = { ['codestyle-check'] = "any" }
       },
-    },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        }
+        -- This causes slow init of sumneko_lua
+        -- library = vim.api.nvim_get_runtime_file("", true),
+      },
+    }
   }
 }
 
-local ccls_settings = {
+__settings['ccls'] = {
   init_options = {
     codeLens = {
       enabled = false,
@@ -57,59 +87,33 @@ local ccls_settings = {
   }
 }
 
--- enables snippet support
 local function make_config()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
+  -- enables snippet support
   capabilities.textDocument.completion.completionItem.snippetSupport = true
+  -- enables LSP autocomplete
   if pcall(require, 'cmp_nvim_lsp') then
     capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
   end
   return {
-    on_attach = on_attach,
+    on_attach = require('lsp.on_attach').on_attach,
     capabilities = capabilities,
   }
 end
 
--- manually installed LSP servers
--- local servers = { 'ccls', 'rust_analyzer' }
-local servers = { 'clangd', 'rust_analyzer' }
-for _, srv in ipairs(servers) do
-  local cfg = make_config()
-  if srv == 'ccls' then
-    cfg = vim.tbl_deep_extend("force", ccls_settings, cfg)
-  end
-  require("lspconfig")[srv].setup(cfg)
-end
-
-local lsp_installer = require("nvim-lsp-installer")
-
-lsp_installer.settings {
-  ui = {
-    icons = {
-      server_installed = "✓",
-      server_pending = "➜",
-      server_uninstalled = "✗"
-    }
-  }
+local servers = {
+  'sumneko_lua',
+  'rust_analyzer',
+  'gopls',
+  'pylsp',
+  'clangd',
+  -- 'ccls',
 }
 
-lsp_installer.on_server_ready(function(server)
-    local opts = make_config()
-
-    if server.name == "sumneko_lua" then
-      opts.settings = lua_settings
-      -- Enables formatting, see:
-      -- https://github.com/sumneko/lua-language-server/issues/960
-      -- https://github.com/sumneko/lua-language-server/wiki/Code-Formatter
-      opts.cmd = { 'lua-language-server', '--preview' }
-      opts.root_dir = function(path)
-        return require'lspconfig.util'.root_pattern({".git", ".sumneko_lua"})(path)
-          or vim.fn.expand('%:h')
-      end
-    end
-
-    -- This setup() function is exactly the same as
-    -- lspconfig's setup function (:help lspconfig-quickstart)
-    server:setup(opts)
-    vim.cmd [[ do User LspAttachBuffers ]]
-end)
+for _, srv in ipairs(servers) do
+  local cfg = make_config()
+  if __settings[srv] then
+    cfg = vim.tbl_deep_extend("force", __settings[srv], cfg)
+  end
+  lspconfig[srv].setup(cfg)
+end
