d76faf5197fc93d4808c0bbb209cd4a2233bf33a
bhagwan <bhagwan@disroot.org>
2022-05-25 12:25:14 -0700

    mini.surround: improved left bracket customizations

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -9,10 +9,26 @@ surround.setup({
   -- 'ysiw('    foo -> ( foo )
   -- 'ysiw)'    foo ->  (foo)
   custom_surroundings = {
-    ['('] = { output = { left = '( ', right = ' )' } },
-    ['['] = { output = { left = '[ ', right = ' ]' } },
-    ['{'] = { output = { left = '{ ', right = ' }' } },
-    ['<'] = { output = { left = '< ', right = ' >' } },
+    -- ['('] = { output = { left = '( ', right = ' )' } },
+    -- ['['] = { output = { left = '[ ', right = ' ]' } },
+    -- ['{'] = { output = { left = '{ ', right = ' }' } },
+    -- ['<'] = { output = { left = '< ', right = ' >' } },
+    ['('] = {
+      input = { find = '%(%s-.-%s-%)', extract = '^(.%s*).-(%s*.)$' },
+      output = { left = '( ', right = ' )' },
+    },
+    ['['] = {
+      input = { find = '%[%s-.-%s-%]', extract = '^(.%s*).-(%s*.)$' },
+      output = { left = '[ ', right = ' ]' },
+    },
+    ['{'] = {
+      input = { find = '{%s-.-%s-}', extract = '^(.%s*).-(%s*.)$' },
+      output = { left = '{ ', right = ' }' },
+    },
+    ['<'] = {
+      input = { find = '<%s-.-%s->', extract = '^(.%s*).-(%s*.)$' },
+      output = { left = '< ', right = ' >' },
+    },
     S = {
       -- lua bracketed string mapping
       -- 'ysiwS'  foo -> [[foo]]
@@ -30,7 +46,7 @@ surround.setup({
     update_n_lines = '',  -- bind for updating 'config.n_lines'
   },
   -- Number of lines within which surrounding is searched
-  n_lines = 30,
+  n_lines = 62,
 
   -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
   highlight_duration = 2000,
@@ -43,6 +59,13 @@ surround.setup({
 
 -- Remap adding surrounding to Visual mode selection
 vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+
+-- unmap config generated `ys` mapping, prevents visual mode yank delay
+if vim.keymap then
+  vim.keymap.del("x", "ys")
+else
+  vim.cmd("xunmap ys")
+end
 
 -- Make special mapping for "add surrounding for line"
 vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false })
