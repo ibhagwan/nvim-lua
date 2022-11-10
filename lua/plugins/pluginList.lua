03d7f88dc6e57b4357a3bd7cf9daca6af9c9d859
bhagwan <bhagwan@disroot.org>
2022-11-09 19:31:00 -0500

    disable null-ls spell completions, updated enum LSP icon

--- lua/plugins/pluginList.lua
+++ lua/plugins/pluginList.lua
@@ -187,7 +187,7 @@ use({
       require("null-ls").setup({
         sources = {
           require("null-ls").builtins.formatting.stylua,
-          require("null-ls").builtins.completion.spell,
+          --require("null-ls").builtins.completion.spell,
         },
       })
     ]],
