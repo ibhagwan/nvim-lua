be0b8b07e7aa24b4d55697115ec6abeda7efd556
bhagwan <bhagwan@disroot.org>
2023-02-26 22:42:10 -0800

    feat: added `mini.pairs`

--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -4,13 +4,14 @@ local M = {
   "echasnovski/mini.nvim",
   -- not using "VeryLazy" event as it bugs out the splashscreen
   -- https://github.com/echasnovski/mini.nvim/issues/238
-  event = "BufReadPost",
+  event = { "BufReadPost", "InsertEnter" }
 }
 
 function M.config()
   require("plugins.mini.surround")
   require("plugins.mini.indentscope")
   require("mini.ai").setup()
+  require("mini.pairs").setup()
 end
 
 return M
