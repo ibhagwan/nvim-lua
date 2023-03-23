a952d46fdfc969b592947269b16f8469744d5cbf
bhagwan <bhagwan@disroot.org>
2023-03-23 16:55:57 -0700

    feat: added keybind for fzf-lua new `lsp_finder`

--- lua/plugins/fzf-lua/mappings.lua
+++ lua/plugins/fzf-lua/mappings.lua
@@ -120,6 +120,7 @@ map_fzf("n", "<leader>fo", "colorschemes", {
 })
 
 -- LSP
+map_fzf("n", "<leader>lf", "lsp_finder", { desc = "location finder [LSP]" })
 map_fzf("n", "<leader>lr", "lsp_references", { desc = "references [LSP]" })
 map_fzf("n", "<leader>ld", "lsp_definitions",
   { desc = "definitions [LSP]", jump_to_single_result = false })
