cffed8810e29664213611d634e4c83b6b0ea0896
bhagwan <bhagwan@disroot.org>
2023-06-23 21:50:22 -0700

    feat: fzf-lua `register_ui_select` dynamic height

--- lua/plugins/fzf-lua/init.lua
+++ lua/plugins/fzf-lua/init.lua
@@ -12,13 +12,16 @@ function M.config()
   require("plugins.fzf-lua.setup").setup()
 
   -- register fzf-lua as vim.ui.select interface
-  require("fzf-lua").register_ui_select({
-    winopts = {
-      height = 0.30,
-      width  = 0.70,
-      row    = 0.40,
-    }
-  })
+  require("fzf-lua").register_ui_select(function(_, items)
+    local min_h, max_h = 0.15, 0.70
+    local h = (#items + 4) / vim.o.lines
+    if h < min_h then
+      h = min_h
+    elseif h > max_h then
+      h = max_h
+    end
+    return { winopts = { height = h, width = 0.60, row = 0.40 } }
+  end)
 end
 
 return M
